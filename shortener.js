5157e8f1f92f7d902cb6a46b433eb95d61c81beb
Ata Berk YILMAZ <ataberkylmz@MacAir.local>
2018-07-23 17:59:17 +0300

    V1.1.0 - basic

--- shortener.js
+++ shortener.js
@@ -4,10 +4,12 @@ const BodyParser = require('body-parser');
 const path = require('path');
 const LokiDB = require('lokijs');
 const rand = require('random-js');
+const fs = require('fs');
+const conf = require('./config.json');
 
 const app = Express();
 let links;
-// initialize database
+// Initialize the database.
 const db = new LokiDB('db', {
   autoload: true,
   autoloadCallback: () => {
@@ -23,41 +25,62 @@ const db = new LokiDB('db', {
 app.use(BodyParser.json());
 app.use(BodyParser.urlencoded({ extended: true }));
 
+// Send the index file.
 app.get('/', (req, res) => res.sendFile(path.join(`${__dirname}/index.html`)));
 
 app.post('/new', (req, res) => {
   let mainUrl = req.body.targetUrl;
+  // Check if the posted URL contains an http prefix.
   if (!mainUrl.startsWith('http')) {
+    // If it doesn't contain an http prefix, add http prefix to the posted URL.
     mainUrl = `http://${mainUrl}`;
   }
 
   console.log(`New request received. Url to shorten is ${req.body.targetUrl}`);
+  // Chech if the posted URL is already shortened or not.
   const tryGet = links.findOne({ url: mainUrl });
   if (tryGet) {
+    // if it is shortened already, show the existing url.
+    // IMPLEMENT!
     console.log(`Short URL already exist in the database. ID: ${tryGet.short}`);
   } else {
-    const shortUrlId = rand.string('qwertyuopasdfghjklizxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM1234567890')(rand.engines.mt19937().autoSeed(), 6);
+    // If it is not shortened already, generate a new random string with a lenght of 4.
+    // There should be 21381376 unique adress with lenght of 4.
+    // If we use 3 charactes instead of 4, there should be 314432 unique combination.
+    let shortUrlId = 'aaaa';
+    do {
+      shortUrlId = rand.string('qwertyuopasdfghjklizxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM1234567890')(rand.engines.mt19937().autoSeed(), 4);
+    } while (links.findOne({ url: mainUrl }));
     links.insert({
+      // Insert the newly generated url into the database with expiraton date of 1 month.
       url: mainUrl, short: shortUrlId, count: 0, expire: Date.now() + 60 * 60 * 24 * 30,
     });
     console.log(`New shout created. Url id is ${shortUrlId}`);
   }
-
   const queryRes = links.findOne({ url: mainUrl }).short;
+  fs.readFile(`${__dirname}/created.html`, 'utf8', (err, text) => {
+    const newText = text.replace('REPLACETHIS', `${conf.HOST}:${conf.PORT}/${queryRes}`);
+    res.send(newText);
+  });
 
-  res.send(`${mainUrl} => ${queryRes}`);
+  // const htmlNew = htmlOld.replace('REPLACETHIS', queryRes);
+  // res.send(htmlNew);
 });
 
 app.get('/:shortId', (req, res) => {
   try {
     const tryGet = links.findOne({ short: req.params.shortId });
-    const redirUrl = tryGet.url;
-    console.log(redirUrl);
-    res.redirect(302, `${redirUrl}`);
-    res.end();
+    if (!tryGet) {
+      res.redirect(301, '/');
+    } else {
+      const redirUrl = tryGet.url;
+      console.log(redirUrl);
+      res.redirect(302, `${redirUrl}`);
+    }
   } catch (error) {
-    console.error('Hata');
+    console.error('Error while getting the url from the server.');
+    res.redirect(404, '');
   }
 });
 
-app.listen(3000, () => console.log('Server is listening on port 3000...'));
+app.listen(conf.PORT, () => console.log(`Server is listening on port ${conf.PORT}...`));
